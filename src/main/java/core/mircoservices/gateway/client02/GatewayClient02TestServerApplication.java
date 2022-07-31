a9015e2d17465282c5cfb4bef74c5ab6eb65d28d
nhut_pham <nhut.pham@ilogic.vn>
2022-07-31 14:53:12 +0700

    init proj

---
+++ src/main/java/core/mircoservices/gateway/client02/GatewayClient02TestServerApplication.java
@@ -0,0 +1,13 @@
+package core.mircoservices.gateway.client02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayClient02TestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayClient02TestServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/mircoservices/gateway/client02/GatewayClient02TestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package core.mircoservices.gateway.client02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayClient02TestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
